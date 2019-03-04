4cbb57a1546ec550695d63a29d08e9b608b8c09d
Ailson F. dos Santos <ailsonforte@hotmail.com>
2019-03-04 15:08:56 -0300

    Client interface methods added

--- client/src/main/java/br/ufrn/imd/dim0614/servidor_de_eventos/client/Client.java
+++ client/src/main/java/br/ufrn/imd/dim0614/servidor_de_eventos/client/Client.java
@@ -7,28 +7,112 @@ import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
+import java.util.List;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import br.ufrn.imd.dim0614.servidor_de_eventos.classes.Event;
+import br.ufrn.imd.dim0614.servidor_de_eventos.classes.User;
 import br.ufrn.imd.dim0614.servidor_de_eventos.interfaces.Server;
 
 /**
  * @author Ailson Forte dos Santos
  *
  */
-public class Client {
+public class Client implements br.ufrn.imd.dim0614.servidor_de_eventos.interfaces.Client {
+	
+	private static Server server;
+
+	/* (non-Javadoc)
+	 * @see br.ufrn.imd.dim0614.servidor_de_eventos.interfaces.Client#createEvent(br.ufrn.imd.dim0614.servidor_de_eventos.classes.Event)
+	 */
+	public boolean createEvent(Event event) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see br.ufrn.imd.dim0614.servidor_de_eventos.interfaces.Client#createEvent(java.lang.String, java.util.List, java.lang.String)
+	 */
+	public boolean createEvent(String name, List<String> topics, String description) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see br.ufrn.imd.dim0614.servidor_de_eventos.interfaces.Client#addInterestTopic(br.ufrn.imd.dim0614.servidor_de_eventos.classes.User, java.lang.String)
+	 */
+	public boolean addInterestTopic(User user, String topic) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see br.ufrn.imd.dim0614.servidor_de_eventos.interfaces.Client#createUser(java.lang.String, java.lang.String, java.util.List)
+	 */
+	public User createUser(String userName, String name, List<String> interests) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see br.ufrn.imd.dim0614.servidor_de_eventos.interfaces.Client#checkNotifications(br.ufrn.imd.dim0614.servidor_de_eventos.classes.User)
+	 */
+	public List<Event> checkNotifications(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Options options = generateArgsOpts();
+		CommandLineParser parser = new DefaultParser();
+		HelpFormatter formatter = new HelpFormatter();
+		CommandLine cmd;
+		
 		try {
-			Server server = (Server) Naming.lookup("rmi://localhost:1900/EventServer");
+			cmd = parser.parse(options, args);
+			String ipAddress = cmd.getOptionValue("ip");
+			
+			ipAddress = (ipAddress.isEmpty()?"localhost":ipAddress);
+
+			System.out.println(ipAddress);
+			
+			server = (Server) Naming.lookup("rmi://" + ipAddress + ":1900/EventServer");
+			
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		} catch (NotBoundException e) {
 			e.printStackTrace();
+		} catch (ParseException e) {
+			System.out.println(e.getMessage());
+			formatter.printHelp("utility-name", options);
+			e.printStackTrace();
 		}
+
+	}
+	
+	/**
+	 * @return org.apache.commons.cli.Options object with all required CLI options
+	 */
+	private static final Options generateArgsOpts() {
+		Options options = new Options();
+		Option ip = new Option("i", "ip", true, "Serve ip address");
+		ip.setType(String.class);
+		ip.setRequired(false);
+		options.addOption(ip);
+
+		return options;
 	}
 
 }
--- domain/src/main/java/br/ufrn/imd/dim0614/servidor_de_eventos/interfaces/Client.java
+++ domain/src/main/java/br/ufrn/imd/dim0614/servidor_de_eventos/interfaces/Client.java
@@ -1,7 +1,41 @@
 package br.ufrn.imd.dim0614.servidor_de_eventos.interfaces;
 
+import java.util.List;
+
+import br.ufrn.imd.dim0614.servidor_de_eventos.classes.Event;
+import br.ufrn.imd.dim0614.servidor_de_eventos.classes.User;
+
 import java.rmi.Remote;
 
 public interface Client extends Remote {
+
+	/**
+	 * @param event
+	 * @return
+	 */
+	public boolean createEvent(Event event);
+
+	/**
+	 * @param event
+	 * @return
+	 */
+	public boolean createEvent(String name, List<String> topics, String description);
+
+	/**
+	 * @param user
+	 * @return
+	 */
+	public boolean addInterestTopic(User user, String topic);
+
+	/**
+	 * @return
+	 */
+	public User createUser(String userName, String name, List<String> interests);
+
+	/**
+	 * @param user
+	 * @return
+	 */
+	public List<Event> checkNotifications(User user);
 
 }
