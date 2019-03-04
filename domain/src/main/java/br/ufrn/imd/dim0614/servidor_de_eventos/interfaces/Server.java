c9126e249addc5ae2839e0519301d3ae406bbf23
Ailson F. dos Santos <ailsonforte@hotmail.com>
2019-03-04 09:34:21 -0300

    Creation of interface methods

--- domain/src/main/java/br/ufrn/imd/dim0614/servidor_de_eventos/interfaces/Server.java
+++ domain/src/main/java/br/ufrn/imd/dim0614/servidor_de_eventos/interfaces/Server.java
@@ -5,6 +5,9 @@ package br.ufrn.imd.dim0614.servidor_de_eventos.interfaces;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.List;
+
+import br.ufrn.imd.dim0614.servidor_de_eventos.classes.Event;
 
 /**
  * @author Ailson Forte dos Santos
@@ -12,10 +15,13 @@ import java.rmi.RemoteException;
  */
 public interface Server extends Remote {
 
-	public boolean createEvent(String name, String topics, String description) throws RemoteException;
-	public boolean publishEvent() throws RemoteException;
-	public boolean groupEvent() throws RemoteException;
-	public boolean addInterestTopic() throws RemoteException;
+	public Event createEvent(Event event) throws RemoteException;
+	public Event createEvent(String name, List<String> topics, String description) throws RemoteException;
+	public boolean publishEvent(Event event) throws RemoteException;
+	public boolean publishEvent(String eventName) throws RemoteException;
+	public List<Event> groupEvent(String topic) throws RemoteException;
+	public List<Event> groupEvent(List<String> topic) throws RemoteException;
+	public boolean addInterestTopic(String userName, String topic) throws RemoteException;
 	public boolean notifyEvent() throws RemoteException;
 
 }
